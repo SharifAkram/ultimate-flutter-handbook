2726e6d968c2c4fd9688e3cfc4f2203a35d42c6a
Sharif Akram <rifkit@gmail.com>
2023-11-17 18:56:55 -0500

    second commit - test

---
+++ 03/listView.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ListView(
+            children: const [
+              ListTile(
+                leading: Icon(Icons.map),
+                title: Text('Map'),
+              ),
+              ListTile(
+                leading: Icon(Icons.photo_album),
+                title: Text('Album'),
+              ),
+              ListTile(
+                leading: Icon(Icons.phone),
+                title: Text('Phone'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
