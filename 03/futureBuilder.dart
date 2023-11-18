a5b01b73498e2cbaf6b8b30997a9d307d899a227
Sharif Akram <rifkit@gmail.com>
2023-11-17 19:25:47 -0500

    add code samples from ch 3

--- 03/futureBuilder.dart
+++ 03/futureBuilder.dart
@@ -1,1 +1,32 @@
 import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    Future fetchUserData() async {
+      await Future.delayed(const Duration(seconds: 2));
+    }
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: FutureBuilder(
+            future: fetchUserData(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                // build your widget based on the data returned by the future
+                return Text('Hello, ${snapshot.data['name]}!');
+              } else if (snapshot.hasError) {
+                // handle any errors that ocurred while fetching the data
+                return Text('Error: ${snapshot.error}');
+              } else {
+                // show a loading indicator while the data is being fetched
+                return CircularProgressIndicator();
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
--- 03/streamBuilder.dart
+++ 03/streamBuilder.dart
@@ -1,1 +1,38 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+class _MainAppState extends State<MainApp> {
+  StreamController<int> _streamController = StreamController<int>(setState(() {
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: StreamBuilder<int>(
+            stream: _streamController.stream,
+            builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+              if (snapshot.hasError) {
+                Return const Text('Error: ${snapshot.error}');
+              }
+              if (!snapshot.hasData) {
+                return Text('No data yet.');
+              }
+              return Text ('Stream value: ${snapshot.data}');
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _streamController.sink.add(DateTime.now().millisecondsSinceEpoch);
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
